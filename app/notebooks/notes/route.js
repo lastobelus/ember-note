f06e036d2ee3f86f969e60a6547d528dfc7ba32d
Michael Johnston <lastobelus@mac.com>
2015-11-01 17:37:27 -0800

    add notebooks/notes
    ember generate route notebooks/notes --pod
    addNote action in notebooks/notes route

---
+++ app/notebooks/notes/route.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    return this.store.query('note', { notebook: params.notebook_id });
+  },
+
+  actions: {
+    addNote: function() {
+      this.store.findRecord('notebook',
+        this.paramsFor('notebook.notes').notebook_id).then((notebook) => {
+          console.log('notebook', notebook);
+          var note = this.store.createRecord('note', {
+            title: this.controller.get("title"),
+            notebook: notebook
+          });
+          console.log('note', note);
+          note.save().then(() => {
+            console.log('note save successful');
+            this.controller.set('title', null);
+            this.refresh();
+          }, function() {
+            console.log('note save failed');
+          });
+      });
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -10,7 +10,11 @@ Router.map(function() {
   this.route('login');
   this.route('notebooks', {
     path: 'notebooks/:user_id'
-  }, function() {});
+  }, function() {
+    this.route('notes', { path: "notes/:notebook_id" }, function() {
+    
+    });
+  });
 });
 
 export default Router;
