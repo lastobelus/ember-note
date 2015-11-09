94db58f5cb4f3c8255bf6c20873c0969426b28bf
Michael Johnston <lastobelus@mac.com>
2015-11-08 21:21:47 -0800

    back to normal api and add utils/is-valid-length
    chapter 7

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api/alt',
+  namespace: 'api',
   headers: function () {
     if (this.get('session.user')) {
       return {
--- app/notebooks/notes/route.js
+++ app/notebooks/notes/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import isValidLength from 'ember-note/utils/is-valid-length'
 
 export default Ember.Route.extend({
   model: function(params) {
@@ -7,22 +8,29 @@ export default Ember.Route.extend({
 
   actions: {
     addNote: function() {
-      this.store.findRecord('notebook',
-        this.paramsFor('notebook.notes').notebook_id).then((notebook) => {
-          console.log('notebook', notebook);
-          var note = this.store.createRecord('note', {
-            title: this.controller.get("title"),
-            notebook: notebook
-          });
-          console.log('note', note);
-          note.save().then(() => {
-            console.log('note save successful');
-            this.controller.set('title', null);
-            this.refresh();
-          }, function() {
-            console.log('note save failed');
-          });
-      });
+      var title = this.controller.get("title");
+      if ( !isValidLength(title, 0, 140)) {
+        alert('Title must be longer than 0 '
+          + 'characters and not more than 140.'
+        );
+      } else {
+        this.store.findRecord('notebook',
+          this.paramsFor('notebook.notes').notebook_id).then((notebook) => {
+            console.log('notebook', notebook);
+            var note = this.store.createRecord('note', {
+              title: this.controller.get("title"),
+              notebook: notebook
+            });
+            console.log('note', note);
+            note.save().then(() => {
+              console.log('note save successful');
+              this.controller.set('title', null);
+              this.refresh();
+            }, function() {
+              console.log('note save failed');
+            });
+        });        
+      }
     },
     deleteNote(note) {
       console.log('deleting note with title '+note.get('title'));
