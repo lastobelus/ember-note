abb8304c94dc5c72b5fcb6649ab8dab69657f41a
Michael Johnston <lastobelus@mac.com>
2015-11-10 13:50:02 -0800

    minor fixes to get unit tests running
    chapter 8

--- tests/unit/utils/is-valid-length-test.js
+++ tests/unit/utils/is-valid-length-test.js
@@ -4,7 +4,17 @@ import { module, test } from 'qunit';
 module('Unit | Utility | is valid length');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  var result = isValidLength();
+test('it returns true when value is between min and max', function(assert) {
+  var result = isValidLength("12345678", 2, 10);
   assert.ok(result);
+});
+
+test('it returns false when value is above max', function(assert) {
+  var result = isValidLength("12345678901", 2, 10);
+  assert.ok(!result);
+});
+
+test('it returns false when value is below min', function(assert) {
+  var result = isValidLength("1", 2, 10);
+  assert.ok(!result);
 });
