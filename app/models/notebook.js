d01494c7aca731de7aadc4f3b484accf90ce2056
Michael Johnston <lastobelus@mac.com>
2015-11-10 15:13:43 -0800

    use app.bowerDirectory

--- app/models/notebook.js
+++ app/models/notebook.js
@@ -3,5 +3,9 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   user: DS.belongsTo('user'),
-  notes: DS.hasMany('note')
+  notes: DS.hasMany('note'),
+  noteCount() {
+    return this.get('notes.length');
+  }
+  
 });
